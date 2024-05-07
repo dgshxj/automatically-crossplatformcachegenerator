bf9997aff9e11786160d6f23c05b8bf8242c9aa1
dgshxj <gs1isiboy67@hotmail.com>
2024-05-07 08:21:08 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const fibonacci = (n) => {
-  const sequence = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2]);
-  }
-  return sequence;
-};
+const nthElement = (arr, n = 0) =>
+  (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
