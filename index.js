e92501c5fef98a7f24c08f4b3e404301f3d2d008
dgshxj <gs1isiboy67@hotmail.com>
2024-05-08 09:06:15 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const nthElement = (arr, n = 0) =>
-  (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
+function countPrimes(n) {
+  const isPrime = new Array(n).fill(true);
+  isPrime[0] = false;
+  isPrime[1] = false;
+  for (let i = 2; i * i < n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j < n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (isPrime[i]) count++;
+  }
+  return count;
+}
