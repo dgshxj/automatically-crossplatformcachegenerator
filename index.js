2a4d26f38a72d3da6f0bf67bab2feac650e62fe6
dgshxj <gs1isiboy67@hotmail.com>
2024-05-10 07:12:32 +0700

    First commit

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-function multiply(num1, num2) {
-  const m = num1.length;
-  const n = num2.length;
-  const pos = new Array(m + n).fill(0);
-  for (let i = m - 1; i >= 0; i--) {
-    for (let j = n - 1; j >= 0; j--) {
-      const mul = (num1[i] - "0") * (num2[j] - "0");
-      const p1 = i + j;
-      const p2 = i + j + 1;
-      const sum = mul + pos[p2];
-      pos[p1] += Math.floor(sum / 10);
-      pos[p2] = sum % 10;
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
     }
   }
-  let result = "";
-  for (const p of pos) {
-    if (!(result.length === 0 && p === 0)) result += p;
-  }
-  return result.length === 0 ? "0" : result;
-}
+  return arr;
+};
